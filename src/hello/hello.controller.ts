fc5c783e0e9da80dd7c0c5d88c95ff259d8804b9
Mohammadhadi Omidi <mohammadhadi.omidi95@gmail.com>
2021-04-19 23:59:12 +0430

    Write controller and service for person dto.

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Header, Query  } from '@nestjs/common';
+import { PersonDto } from './dto/dto.person';
+import { HelloService } from './hello.service';
 
 @Controller('hello')
-export class HelloController {}
+export class HelloController {
+
+    constructor(
+        private readonly helloService: HelloService
+    ) {}
+
+    @Post('welcome')
+    @Header('Content-Type', 'application/json')
+    async sayWelcome (@Body() personDto: PersonDto): Promise<{data : String}>
+    {
+        let msg = await this.helloService.welcome(personDto);
+        return  {data : msg};
+    }
+
+    @Get('welcome')
+    async sayWelcome2 (@Query('name') iName, @Query('year') iYear): Promise<{data : String}>
+    {
+        let msg = await this.helloService.welcome({name: iName, year: iYear});
+        return  {data : msg};
+    }
+
+}
