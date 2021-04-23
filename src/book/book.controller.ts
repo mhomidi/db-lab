def2290fc6b13cdf0d8e27490c3b793de5dc49db
Mohammadhadi Omidi <mohammadhadi.omidi95@gmail.com>
2021-04-23 17:25:07 +0430

    Add delete and update to book.

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put, Query } from '@nestjs/common';
 import { BookService } from './book.service';
 import CreateBookDto from './dto/create-book.dto';
+import UpdateBookDto from './dto/update-book.dto';
 
 @Controller('book')
 export class BookController {
@@ -9,11 +10,22 @@ export class BookController {
 
 
     @Post('post')
-    postGenre( @Body() genre: CreateBookDto) {
-    return this.bookService.insert(genre);
+    postBook( @Body() book: CreateBookDto) {
+        return this.bookService.insert(book);
     }
+
     @Get()
     getAll() {
-    return this.bookService.getAllBooks();
+        return this.bookService.getAllBooks();
+    }
+
+    @Put('update')
+    updateBook( @Body() book: UpdateBookDto) {
+        return this.bookService.update(book);
+    }
+
+    @Delete('delete')
+    deleteBook( @Query("id") id) {
+        return this.bookService.delete(id);
     }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -3,6 +3,7 @@ import { createQueryBuilder, getConnection } from 'typeorm';
 import BookEntity from 'src/db/book.entity';
 import UserEntity from 'src/db/user.entity';
 import GenreEntity from 'src/db/genre.entity';
+import UpdateBookDto from './dto/update-book.dto';
 
 export class BookService {
 
@@ -23,5 +24,36 @@ export class BookService {
   async getAllBooks(): Promise<BookEntity[] > {
     // const user: UserEntity = await UserEntity.findOne({where: {id: 2}, relations: ['books']});
     return BookEntity.find();
+  }
+
+  async update(bookDetails: UpdateBookDto): Promise<BookEntity>
+  {
+    const { id, name , userID , genreIDs } = bookDetails;
+    let entity = BookEntity.findOne(id);
+    if (name != null){
+      (await entity).name = name;
+    }
+
+    if (userID != null){
+      (await entity).user = await UserEntity.findOne(userID) ;
+    }
+
+    if (genreIDs != null){
+      for ( let i = 0; i < genreIDs.length ; i++)
+      {
+        const genre = await GenreEntity.findOne(genreIDs[i]);
+        (await entity).genres.push(genre);
+      }
+    }
+
+    return entity;
+  }
+
+  async delete(id: string): Promise<BookEntity>
+  {
+    const book = BookEntity.findOne(id);
+    (await book).remove();
+
+    return book;
   }
 }
