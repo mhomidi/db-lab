4faadbe08c3e30654a5a15d7e824ec8d4a4bbe9a
Mohammadhadi Omidi <mohammadhadi.omidi95@gmail.com>
2021-04-23 16:52:38 +0430

    Do part 3.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { BooksModule } from './books/books.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { GenreService } from './genre/genre.service';
+import { GenreModule } from './genre/genre.module';
+import { BookController } from './book/book.controller';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [HelloModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HelloModule, BooksModule, UserModule, GenreModule, BookModule],
+  controllers: [AppController, UserController, BookController],
+  providers: [AppService, UserService, GenreService],
 })
 export class AppModule {}
--- src/hello/dto/dto.person.ts
+++ src/hello/dto/dto.person.ts
@@ -4,8 +4,8 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 export class PersonDto {
   @Length(3, 10)
   @ApiProperty({description:'Enter Your Name > ', minLength: 3, default: ' Ali' ,maxLength:10})
-  
   name: string;
+
   @IsNumber()
   @IsOptional()
   @Min(1960)
