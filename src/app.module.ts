7becf83c831ad412ec8a39baef146492c110be23
Mohammadhadi Omidi <mohammadhadi.omidi95@gmail.com>
2021-04-23 16:57:50 +0430

    Update modules file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HelloModule } from './hello/hello.module';
-import { BooksModule } from './books/books.module';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { UserModule } from './user/user.module';
-import { GenreService } from './genre/genre.service';
-import { GenreModule } from './genre/genre.module';
 import { BookController } from './book/book.controller';
-import { BookModule } from './book/book.module';
+import BooksModule from './book/book.module';
+import { BookService } from './book/book.service';
+import GenreController from './genre/genre.controller';
+import GenreModule from './genre/genre.module';
+import GenreService from './genre/genre.service';
+import { HelloModule } from './hello/hello.module';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [HelloModule, BooksModule, UserModule, GenreModule, BookModule],
-  controllers: [AppController, UserController, BookController],
-  providers: [AppService, UserService, GenreService],
+  imports: [HelloModule, BooksModule, UserModule, GenreModule, BooksModule],
+  controllers: [AppController, UserController, BookController, GenreController],
+  providers: [AppService, UserService, GenreService, BookService],
 })
 export class AppModule {}
